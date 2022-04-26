df79dea0d7a309197ff87b8c2c6de6fdc437ab7c
MikhailKarpov1997 <misha.kmg@gmail.com>
2022-04-26 21:50:37 +0300

    task is done

---
+++ src/ex11_js_dom/script.js
@@ -0,0 +1,21 @@
+const btnLeft = document.querySelector('.left-button');
+const btnRight = document.querySelector('.right-button');
+const images = ['pic_1.jpeg', 'pic_2.jpg', 'pic_3.jpg', 'pic_4.jpg', 'pic_5.jpg'];
+let i = 0;
+const image = document.querySelector('img');
+image.setAttribute('src', images[i]);
+
+btnLeft.onclick = function () {
+  i -= 1;
+  if (i < 0) {
+    i = images.length - 1;
+  }
+  image.setAttribute('src', images[i]);
+};
+btnRight.onclick = function () {
+  i += 1;
+  if (i >= images.length) {
+    i = 0;
+  }
+  image.setAttribute('src', images[i]);
+};
